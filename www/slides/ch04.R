018d716d6cc193099f7c648503512bfc046dc250
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2009-10-13 22:37:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/engrexpt@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/slides/ch04.R
+++ www/slides/ch04.R
@@ -76,3 +76,112 @@ mean(gsamp <- rgeom(1000000, prob = 0.03)) # should be near 32.3
 var(gsamp)  # should be near 0.97/0.03^2 = 1077.778
 
 
+###################################################
+### chunk number 13: unifcdfplot
+###################################################
+eps <- 100*.Machine$double.eps
+y <- c(12.5,13-eps, 13.0,15.0,15 + eps, 15.5)
+print(xyplot(dunif(y, min = 13, max = 15) + punif(y, min = 13, max = 15) ~ y,
+             outer = TRUE, type = c("g","l"), xlab = "y", ylab = NULL, layout = c(1,2),
+             scales = list(y = list(relation = "free"), x = list(axs = "i"))),
+      pos = c(0,0,0.5,1), more = TRUE)
+y <- c(-0.1,-eps, 0,1,1+eps, 1.1)
+print(xyplot(dunif(y, min = 0, max = 1) + punif(y, min = 0, max = 1) ~ y,
+             outer = TRUE, type = c("g","l"), xlab = "y", ylab = NULL, layout = c(1,2),
+             scales = list(y = list(relation = "free"), x = list(axs = "i"))),
+      pos = c(0.5,0,1,1))
+
+
+###################################################
+### chunk number 14: pexp
+###################################################
+pexp(3.5, rate = 1/7)
+
+
+###################################################
+### chunk number 15: exp6
+###################################################
+1 - pexp(6, rate = 1/7)
+pexp(6, 1/7, lower = FALSE)
+
+
+###################################################
+### chunk number 16: exp3.5
+###################################################
+pexp(9, 1/7) - pexp(3.5, 1/7)
+diff(pexp(c(3.5, 9), 1/7))
+
+
+###################################################
+### chunk number 17: stdnormdensshow eval=FALSE
+###################################################
+## xyplot(dnorm(z) ~ z, type = c("g","l"))
+
+
+###################################################
+### chunk number 18: stdnormdens
+###################################################
+z <- seq(-3.65, 3.65, 0.01)
+print(xyplot(dnorm(z) ~ z, type = c("g","l"), scales = list(x = list(axs = 'i'))))
+
+
+###################################################
+### chunk number 19: IQnormdens
+###################################################
+x <- seq(47, 153, 0.5)
+print(xyplot(dnorm(x, mean = 100, sd = 15) ~ x, type = c("g","l"), scales = list(x = list(axs = 'i'))))
+
+
+###################################################
+### chunk number 20: IQnormcdf
+###################################################
+print(xyplot(pnorm(x, mean = 100, sd = 15) ~ x, type = c("g","l"), scales = list(x = list(axs = 'i'))))
+
+
+###################################################
+### chunk number 21: pnorm
+###################################################
+pnorm(500, 450, 30)
+1 - pnorm(550, 450, 30)
+diff(pnorm(c(400,500), 450, 30))
+
+
+###################################################
+### chunk number 22: qnorm
+###################################################
+qnorm(0.98, 100, 15)
+
+
+###################################################
+### chunk number 23: alumdens
+###################################################
+print(densityplot(~ ppm, alum, xlab = "Parts per million"),
+      split = c(1,1,1,2), more = TRUE)
+print(densityplot(~ ppm, alum, scales = list(x = list(log = 2)),
+                  xlab = "Parts per million (logarithmic scale)"),
+      split = c(1,2,1,2))
+
+
+###################################################
+### chunk number 24: pars
+###################################################
+with(alum, c(muhat = mean(log(ppm)), sigmahat = sd(log(ppm))))
+
+
+###################################################
+### chunk number 25: ex4.3.20
+###################################################
+plnorm(100, meanlog = 5.0999591, sdlog = 0.6958257)
+pnorm(log(100), mean = 5.0999591, sd = 0.6958257)
+
+
+###################################################
+### chunk number 26: weibullplot
+###################################################
+w <- seq(0, 5, 0.02)
+show(xyplot(dweibull(w, 0.5, 1) + dweibull(w, 1, 1) + dweibull(w, 1.5, 1) + dweibull(w, 3, 1) ~ w,
+            type = c("g","l"),
+            outer = FALSE, ylab = NULL, xlab = NULL,
+            auto.key = list(columns = 4, lines = TRUE, points = FALSE)))
+
+
